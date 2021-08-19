14ae5b01cd4038c29a5dc6c55e20fd59e927d5dd
unknown <lontseliel@gmail.com>
2021-08-19 14:47:32 +0100

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>About Us | Hello City</title>
-    </head>
-    <body>
-        <p>Built with &hearts; by LES TEACHERS DU NET.</p>
+@extends('app')
 
-        <p><a href="/">Return to the home page</a></p>
-        <footer>
-            <p>&copy; Copyright {{ date('Y') }}</p>
-        </footer>
-    </body>
-</html>
+@section('content')
+    <p>Built with &hearts; by LES TEACHERS DU NET.</p>
+
+    <p><a href="/">Return to the home page</a></p>
+@endsection
+    
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Hello City</title>
-    </head>
-    <body>
-        <h1>Hello from Quebec!</h1>
+@extends('app')
 
-        <p>It's currently {{ date('h:i A') }}.</p>
+@section('content')
+    <h1>Hello from Quebec!</h1>
 
-        <footer>
-            <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About Us</a></p>
-        </footer>
-    </body>
-</html>
+    <p>It's currently {{ date('h:i A') }}.</p>
+@endsection
