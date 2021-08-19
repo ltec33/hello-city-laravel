27598d59af2939fe8907707aadb994e59753b61c
unknown <lontseliel@gmail.com>
2021-08-19 17:40:01 +0100

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,8 @@
 @section('title', 'About Us | ' .config('app.name'))
 
 @section('content')
+<img src="/images/images.jpg" alt="images Logo">
+
     <p>Built with &hearts; by LES TEACHERS DU NET.</p>
 
     <p><a href="{{ route('home') }}">Return to the home page</a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,8 @@
 @section('title', env('APP_NAME'))
 
 @section('content')
+    <img src="/images/USA.jpg" alt="USA Flag">
+
     <h1>Hello from Quebec!</h1>
 
     <p>It's currently {{ date('h:i A') }}.</p>
